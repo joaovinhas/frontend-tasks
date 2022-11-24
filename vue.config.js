df49390ff6f04631b34eb03089338e12a73d44cd
joaovinhas <joaovitorvinhas22@gmail.com>
2022-11-24 17:46:09 -0400

    Correcao Vue build

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  devServer: {
+    proxy: 'http://localhost:80'
+  },
+
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/tasks/'
+    : '/'
+    ,
+
   transpileDependencies: true
 })
