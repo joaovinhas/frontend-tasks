a3563b09ff3d8bc56ab88c36394c13133548068b
joaovinhas <joaovitorvinhas22@gmail.com>
2022-12-05 22:03:55 -0400

    Correcoes save all tasks

--- src/views/tasks/TasksView.vue
+++ src/views/tasks/TasksView.vue
@@ -33,7 +33,7 @@
         </div>
       </div>
 
-      <div v-if="tasks.root_tasks">
+      <div v-if="c_tasks">
         <ChildTasks
           v-for="(child, index) in c_tasks" :key="index"
           :tasks="child"
@@ -125,9 +125,13 @@
             
             this.tasks = response
 
-            this.childtasks()
+            if(response.child_tasks.length != 0){
+              this.tasks = this.childtasks(this.tasks.root_tasks, this.tasks.child_tasks)
+              this.c_tasks = this.tasks
+            }else{
+              this.c_tasks = this.tasks.root_tasks
+            }
 
-            this.c_tasks = this.tasks.root_tasks
           }
 
       },
@@ -243,194 +247,255 @@
       },
 
 //Salvar Tasks
-      
+
       async salvar_tasks(){
+
         this.old_tasks = await Axios.all_tasks(this.token)
 
-        if(this.c_tasks.length){
+        if(this.old_tasks.root_tasks.length){
 
+          if(this.old_tasks.child_tasks.length){
+            
+            this.old_tasks = this.childtasks(this.old_tasks.root_tasks, this.old_tasks.child_tasks) 
+          }else{
+            this.old_tasks = this.old_tasks.root_tasks
+          }
+
+          this.comparation_tree_tasks()
+        }
+
+      },
+
+      comparation_tree_tasks(){
+
+        for(let x = 0; x < this.old_tasks.length; x++){
+
+          var check_task = false
+          
           for(let i = 0; i < this.c_tasks.length; i++){
 
-            if(this.c_tasks[i].id){
+            if(this.c_tasks[i].id == this.old_tasks[x].id ){
 
-            //Tasks editadas
+//////////////Edited tasks////////////////////////////////////
 
-              for(let x = 0; x < this.old_tasks.root_tasks.length; x++){
+              if(this.c_tasks[i].task != this.old_tasks[x].task){
 
-                if(this.c_tasks[i].id == this.old_tasks.root_tasks[x].id){
-
-                  if(this.c_tasks[i].task != this.old_tasks.root_tasks[x].task){
-                    if(this.edited_tasks == ""){
-                      this.edited_tasks = [this.c_tasks[i]]
-                    }else{
-                      this.edited_tasks.push(this.c_tasks[i])
-                    }
-                  }
-
-                  if(this.c_tasks[i].concluded != this.old_tasks.root_tasks[x].concluded){
-                    if(this.edited_tasks == ""){
-                      this.edited_tasks = [this.c_tasks[i]]
-                    }else{
-                      this.edited_tasks.push(this.c_tasks[i])
-                    }
-                  }
-
+                if(this.edited_tasks == ""){
+                  this.edited_tasks = [this.c_tasks[i]]
+                }else{
+                  this.edited_tasks.push(this.c_tasks[i])
                 }
 
               }
 
-            }else{
+              if(this.c_tasks[i].concluded != this.old_tasks[x].concluded){
+
+                if(this.edited_tasks == ""){
+                  this.edited_tasks = [this.c_tasks[i]]
+                }else{
+                  this.edited_tasks.push(this.c_tasks[i])
+                }
+
+              }
+
+/////////////Deletar children tasks////////////////////////////////
+              if(this.old_tasks[x].children){
+
+                if(this.c_tasks[i].children){
+////////////Children tasks/////////////////////////////////////////
+                  this.comparation_tree_children(this.c_tasks[i].children, this.old_tasks[x].children)
+
+                }else{
+                  var children_tasks = this.old_tasks[x].children
+                  for(let z = 0; z < children_tasks.length; z++){
+
+                    if(this.del_tasks == ""){
+                      this.del_tasks = [children_tasks[z]]
+                    }else{
+                      this.del_tasks.push(children_tasks[z])
+                    }
+
+                  }
+                }
+
+              }else{
+                if(this.c_tasks[i].children){
+////////////Children tasks/////////////////////////////////////////
+                  this.comparation_tree_children(this.c_tasks[i].children, this.old_tasks[x].children)
+
+                }
+              }
+
+              check_task = true
+
+            }
+
+////////////Tasks root novas //////////////////////////////////////
+
+            if(!this.c_tasks[i].id){
               if(this.new_tasks == ""){
                 this.new_tasks = [this.c_tasks[i]]
               }else{
-                this.new_tasks.push(this.c_tasks[i])
-              }
-            }
-
-            if(this.c_tasks[i].children){
-              this.search_tree_save_add(this.c_tasks[i].children)
-            }
-
-          }
-
-        }
-
-    //Select tasks para deletar
-
-        for(let i = 0; i < this.old_tasks.root_tasks.length; i++){
-
-          var check_task = false
-
-          for(let x = 0; x < this.c_tasks.length; x++){
-
-            if(this.old_tasks.root_tasks[i].id == this.c_tasks[x].id){
-              check_task = true
-
-              if(this.c_tasks[x].children){
-
-                this.search_tree_save_del(this.c_tasks[x].children)
-
-              }else{
-                for(let y = 0; y < this.old_tasks.child_tasks.length; y++){
-                  if(this.old_tasks.child_tasks[y].id_parent == this.old_tasks.root_tasks[i].id){
-                    if(this.del_tasks == ""){
-                      this.del_tasks = [this.old_tasks.child_tasks[y]]
-                    }else{
-                      this.del_tasks.push(this.old_tasks.child_tasks[y])
-                    }
+                var check_task2 = false
+                for(let y = 0; y < this.new_tasks.length; y++){
+                  if(this.c_tasks[i].task == this.new_tasks[y].task){
+                    check_task2 = true
                   }
+                }
+                if(!check_task2){
+                  this.new_tasks.push(this.c_tasks[i])
                 }
               }
 
             }
 
           }
+////////////Deletar tasks //////////////////////////////////////////
 
-          if(check_task == false){
-            if(this.del_tasks == ""){
-              this.del_tasks = [this.old_tasks.root_tasks[i]]
-            }else{
-              this.del_tasks.push(this.old_tasks.root_tasks[i])
+            if(check_task == false){
+
+              if(this.del_tasks == ""){
+                this.del_tasks = [this.old_tasks[x]]
+              }else{
+                this.del_tasks.push(this.old_tasks[x])
+              }
+
             }
-          }
 
         }
 
-        if(this.del_tasks == "" && this.edited_tasks == "" && this.new_tasks == ""){
-          
-          this.notification = new Object()
-          this.notification.error = "Nenhuma alteração encontrada!"
-        
+        console.log(this.new_tasks)
+        console.log(this.edited_tasks)
+        console.log(this.del_tasks)
+
+        this.save_tasks_api()
+
+        this.new_tasks = ""
+        this.edited_tasks = ""
+        this.del_tasks = ""
+
+      },
+
+      comparation_tree_children(c_children, old_children){
+
+        if(old_children){
+
+          for(let x = 0; x < old_children.length; x++){
+
+            var check_task = false
+            
+            for(let i = 0; i < c_children.length; i++){
+
+              if(c_children[i].id == old_children[x].id ){
+
+  //////////////Edited tasks////////////////////////////////////
+
+                if(c_children[i].task != old_children[x].task){
+
+                  if(this.edited_tasks == ""){
+                    this.edited_tasks = [c_children[i]]
+                  }else{
+                    this.edited_tasks.push(c_children[i])
+                  }
+
+                }
+
+                if(c_children[i].concluded != old_children[x].concluded){
+
+                  if(this.edited_tasks == ""){
+                    this.edited_tasks = [c_children[i]]
+                  }else{
+                    this.edited_tasks.push(c_children[i])
+                  }
+
+                }
+
+  /////////////Deletar children tasks////////////////////////////////
+                if(old_children[x].children){
+
+                  if(c_children[i].children){
+  ////////////Children tasks/////////////////////////////////////////
+                    this.comparation_tree_children(c_children[i].children, old_children[x].children)
+
+                  }else{
+                    var children_tasks = old_children[x].children
+                    for(let z = 0; z < children_tasks.length; z++){
+
+                      if(this.del_tasks == ""){
+                        this.del_tasks = [children_tasks[z]]
+                      }else{
+                        this.del_tasks.push(children_tasks[z])
+                      }
+
+                    }
+                  }
+
+                }else{
+                   if(c_children[i].children){
+  ////////////Children tasks/////////////////////////////////////////
+                    this.comparation_tree_children(c_children[i].children, old_children[x].children)
+
+                  }
+                }
+
+                check_task = true
+              }
+
+/////////////Tasks root novas //////////////////////////////////////
+
+              if(!c_children[i].id){
+                if(this.new_tasks == ""){
+                  this.new_tasks = [c_children[i]]
+                }else{
+                  var check_task2 = false
+                  for(let y = 0; y < this.new_tasks.length; y++){
+                    if(c_children[i].task == this.new_tasks[y].task){
+                      check_task2 = true
+                    }
+                  }
+                  if(!check_task2){
+                    this.new_tasks.push(c_children[i])
+                  }
+                }
+
+              }
+
+            }
+/////////////Deletar tasks //////////////////////////////////////////
+
+              if(check_task == false){
+
+                if(this.del_tasks == ""){
+                  this.del_tasks = [old_children[x]]
+                }else{
+                  this.del_tasks.push(old_children[x])
+                }
+
+              }
+
+          }
+
         }else{
 
-          this.save_tasks_api()
+//////Tasks children novas //////////////////////////////////////
+          
+          for(let i = 0; i < c_children.length; i++){
 
-          this.del_tasks = ""
-          this.edited_tasks = ""
-          this.new_tasks = ""
-        }
-
-      },
-
-      search_tree_save_add(children){
-
-        for(let i = 0; i < children.length; i++){
-          if(children[i].id){
-            
-            //Tasks editadas
-
-              for(let x = 0; x < this.old_tasks.child_tasks.length; x++){
-
-                if(children[i].id == this.old_tasks.child_tasks[x].id){
-
-                  if(children[i].task != this.old_tasks.child_tasks[x].task){
-                    if(this.edited_tasks == ""){
-                      this.edited_tasks = [children[i]]
-                    }else{
-                      this.edited_tasks.push(children[i])
-                    }
-                  }
-
-                  if(children[i].concluded != this.old_tasks.child_tasks[x].concluded){
-                    if(this.edited_tasks == ""){
-                      this.edited_tasks = [children[i]]
-                    }else{
-                      this.edited_tasks.push(children[i])
-                    }
-                  }
-
-                }
-
-              }
-
-
-          }else{
             if(this.new_tasks == ""){
-              this.new_tasks = [children[i]]
+              this.new_tasks = [c_children[i]]
             }else{
-              this.new_tasks.push(children[i])
-            }
-          }
-
-          if(children[i].children){
-            this.search_tree_save_add(children[i].children)
-          }
-        }
-
-      },
-//Select child tasks para deletar
-      search_tree_save_del(children){
-
-        for(let x = 0; x < this.old_tasks.child_tasks.length; x++){
-          var check_task = false
-          for(let i = 0; i < children.length; i++){
-
-            if(children[i].children){
-              this.search_tree_save_del(children[i].children)
+              this.new_tasks.push(c_children[i])
             }
 
-            if(this.old_tasks.child_tasks[x].id == children[i].id){
-              check_task = true
-              break
-            }
-            if(children[i].id){
-              check_task = true
-              break
-            }
-
-          }
-
-          if(check_task == false){
-            if(this.del_tasks == ""){
-              this.del_tasks = this.old_tasks.child_tasks[x]
-            }else{
-              this.del_tasks.push( this.old_tasks.child_tasks[x] )
+            if(c_children[i].children){
+              this.comparation_tree_children(c_children[i].children, null)
             }
           }
 
         }
 
       },
+
 
 //Mandar tasks para banco de dados
 
@@ -442,7 +507,8 @@
 
           for(let i = 0; i < this.new_tasks.length; i ++){
 
-            response = await Axios.create_task(this.token, this.new_tasks[i].task, this.new_tasks[i].task_parent)
+            response = Axios.create_task(this.token, this.new_tasks[i].task, this.new_tasks[i].task_parent)
+
           }
 
         }
@@ -451,7 +517,8 @@
 
           for(let i = 0; i < this.edited_tasks.length; i ++){
 
-            response = await Axios.edit_task(this.token, this.edited_tasks[i].id, this.edited_tasks[i].task, this.edited_tasks[i].concluded)
+            response = Axios.edit_task(this.token, this.edited_tasks[i].id, this.edited_tasks[i].task, this.edited_tasks[i].concluded)
+
           }
 
         }
@@ -472,29 +539,25 @@
       },
 
 //Cria a tree de tasks
-      
-      childtasks(){
 
-        if(this.tasks.child_tasks.lenght != 0){
+      childtasks(root_tasks, child_tasks){
 
-          for(let i = 0; i < this.tasks.root_tasks.length; i++){
+        for(let i = 0; i < root_tasks.length; i++){
 
-            for(let x = 0; x < this.tasks.child_tasks.length; x++){
+          for(let x = 0; x < child_tasks.length; x++){
 
-              if(this.tasks.root_tasks[i].id == this.tasks.child_tasks[x].id_parent){
+            if(root_tasks[i].id == child_tasks[x].id_parent){
 
-                this.show_child_tasks(this.tasks.child_tasks[x], this.tasks.child_tasks)
+              this.show_child_tasks(child_tasks[x], child_tasks)
 
-                if(this.tasks.root_tasks[i].children){
+              if(root_tasks[i].children){
 
-                  this.tasks.root_tasks[i].children.push(this.tasks.child_tasks[x])
+                root_tasks[i].children.push(child_tasks[x])
 
-                }else{
+              }else{
 
-                  this.tasks.root_tasks[i].children = Object.assign([this.tasks.child_tasks[x]])
-                
-                }
-
+                root_tasks[i].children = Object.assign([child_tasks[x]])
+              
               }
 
             }
@@ -502,6 +565,9 @@
           }
 
         }
+
+        return root_tasks
+
       },
 
       show_child_tasks(root_tasks, child_tasks){
@@ -510,9 +576,13 @@
 
           if(root_tasks.id == child_tasks[i].id_parent){
 
-            this.show_child_tasks(child_tasks[i], this.tasks.child_tasks)
+            this.show_child_tasks(child_tasks[i], child_tasks)
 
-            root_tasks.children = Object.assign([child_tasks[i]])
+            if(root_tasks.children){
+              root_tasks.children.push(child_tasks[i])
+            }else{
+              root_tasks.children = Object.assign([child_tasks[i]])
+            }
 
           }
 
